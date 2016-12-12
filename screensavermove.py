31b14e3d7c5a87aa00ce33b67bc7c51d946d42a7
johoule <jhoule4464@greenvilleschools.us>
2016-12-12 16:22:15 -0500

    worked on block health

--- screensavermove.py
+++ screensavermove.py
@@ -9,6 +9,7 @@ import intersects
 # Initialze game engine
 pygame.init()
 
+
 # Window
 WIDTH = 1000
 HEIGHT = 800
@@ -30,12 +31,17 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 YELLOW = (255, 255, 0)
 GREEN = (0, 255, 0)
+MINT = (183, 255, 236)
+BLUE = (0, 131, 249)
+DARK_MINT = (0, 103, 111)
 
 # Make a player
-player = [200, 150, 25, 25]
+player = [100, 100, 25, 25]
 player_vx = 0
 player_vy = 0
 player_speed = 5
+
+block = [300, 200, 400, 400, 5]
 
 # Game loop
 done = False
@@ -50,41 +56,67 @@ while not done:
 
 
     # Game logic (Check for collisions, update points, etc.)
-    ''' move the player in horizontal direction'''
-    player[0] += player_vx
-
-    ''' move the player in vertical direction '''
-    player[1] += player_vy
-
-    ''' here is where you should resolve player collisions with screen edges '''
-    TOP = player[1]
-    BOTTOM = player[1] + player[3]
-    LEFT = player[0]
-    RIGHT = player[0] + player[2]
-
-    if TOP <0:
-        player_vy += 5
-    elif BOTTOM > HEIGHT:
-        player_vy -= 5
-
-    if LEFT < 0:
-        player_vx += 5
-    elif RIGHT > WIDTH:
-        player_vx -= 5
-
-
-
     ticks += 1
     if ticks == 60:
         player_vx += 5
         player_vy += 5
+
+
+    ''' move the player in horizontal direction'''
+    player[0] += player_vx
+
+
+    ''' here is where you should resolve player collisions with screen edges '''
+    LEFT = player[0]
+    RIGHT = player[0] + player[2]
+
+    if LEFT < 0:
+        player_vx *= -1
+    elif RIGHT > WIDTH:
+        player_vx *= -1
+
+    block_rect = block[:4]
+    
+    if intersects.rect_rect(player, block_rect):
+        if player_vx > 0:
+            player[0] = block_rect[0] - player[2]
+        else:
+            player[0] = block_rect[0] + block_rect[2]
+        player_vx *= -1
+        block[4] -= 1
+
+    ''' move the player in vertical direction '''
+    player[1] += player_vy
+
+    TOP = player[1]
+    BOTTOM = player[1] + player[3]
+
+    if TOP < 0:
+        player_vy *= -1
+    elif BOTTOM > HEIGHT:
+        player_vy *= -1
+
+
+    if intersects.rect_rect(player, block):
+        if player_vy > 0:
+            player[1] = block[1] - player[3]
+        else:
+            player[1] = block[1] + block[3]
+        player_vy *= -1 
+ 
+  
     
 
 
     #Drawing code (describe picture. it isn't actually drawn yet.)
     screen.fill(BLACK)
 
-    pygame.draw.rect(screen, WHITE, player)
+    pygame.draw.ellipse(screen, BLUE, player)
+
+    if block[4] < 1:
+        pygame.draw.rect(screen, RED, block)
+    else:
+        pygame.draw.rect(screen, DARK_MINT, block)
 
 
     #update screen(actually draw the picture in the window.)
