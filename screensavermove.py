3c1578b72e91ae037199a59f1ed311029c60732f
johoule <jhoule4464@greenvilleschools.us>
2017-01-02 14:10:30 -0500

    played with the game

--- screensavermove.py
+++ screensavermove.py
@@ -37,9 +37,11 @@ DARK_MINT = (0, 103, 111)
 MINT2 = (0, 164, 176)
 MINT3 = (0, 226, 244)
 MINT4 = (113, 244, 255)
+MINT5 = (134, 254, 255)
 
 #font
 font = pygame.font.Font(None, 48)
+font2 = pygame.font.Font(None, 42)
 
 # Make a player
 player = [400, 775, 25, 25]
@@ -53,6 +55,8 @@ win = False
 x_value = -5
 
 score = 0
+
+direction = "Left"
 
 blocks = []
 
@@ -73,8 +77,10 @@ while not done:
             if playing == False:
                 if event.key == pygame.K_LEFT:
                     x_value = -5
+                    direction = "Left"
                 elif event.key == pygame.K_RIGHT:
                     x_value = 5
+                    direction = "Right"
                 if event.key == pygame.K_SPACE:
                     player_vx = x_value
                     player_vy = -5
@@ -138,7 +144,7 @@ while not done:
   
     
     ''' get blocks '''
-    blocks = [b for b in blocks if b[4] > 1]
+    blocks = [b for b in blocks if b[4] > 0]
 
     if len(blocks) == 0:
         win = True
@@ -148,16 +154,33 @@ while not done:
     screen.fill(BLACK)
 
     pygame.draw.ellipse(screen, BLUE, player)
+
+    LorR = font.render("Direction: " + str(direction), 1, WHITE)
+    screen.blit(LorR, [400, 0])
+    
     for b in blocks:
         block_rect = b[:4]
         if b[4] == 5:
             pygame.draw.rect(screen, DARK_MINT, block_rect)
+            bhits = font2.render(str(b[4]), 1, WHITE)
+            screen.blit(bhits, [b[0] + 30, b[1]])
         if b[4] == 4:
             pygame.draw.rect(screen, MINT2, block_rect)
+            bhits = font2.render(str(b[4]), 1, WHITE)
+            screen.blit(bhits, [b[0] + 30, b[1]])
         if b[4] == 3:
             pygame.draw.rect(screen, MINT3, block_rect)
+            bhits = font2.render(str(b[4]), 1, WHITE)
+            screen.blit(bhits, [b[0] + 30, b[1]])
         if b[4] == 2:
             pygame.draw.rect(screen, MINT4, block_rect)
+            bhits = font2.render(str(b[4]), 1, WHITE)
+            screen.blit(bhits, [b[0] + 30, b[1]])
+        if b[4] == 1:
+            pygame.draw.rect(screen, MINT5, block_rect)
+            bhits = font2.render(str(b[4]), 1, WHITE)
+            screen.blit(bhits, [b[0] + 30, b[1]])
+                                 
 
     if win == True:
         winner = font.render("You Win!", 1, WHITE)
